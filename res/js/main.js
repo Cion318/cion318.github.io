dd3c0c844aff998760b69a453661a4c4bb7e5414
Alexander Avercenko <alexander.avercenko@gmail.com>
2023-09-09 13:17:17 +0200

    Reset files.

---
+++ res/js/main.js
@@ -0,0 +1,20 @@
+/*
+document.addEventListener('DOMContentLoaded', function () {
+    // Select all the navigation links inside the navbar
+    const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+  
+    // Add a click event listener to each link
+    navLinks.forEach(function (link) {
+      link.addEventListener('click', function () {
+        // Delay in milliseconds before closing the menu
+        const delay = 500; // Adjust this value as needed
+  
+        // Close the navbar menu after the specified delay
+        setTimeout(function () {
+          const navbarToggler = document.querySelector('.navbar-toggler');
+          navbarToggler.click();
+        }, delay);
+      });
+    });
+  });
+  */
